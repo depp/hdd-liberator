37629105518165a0dbe0c03d73b0835fad5d568e
Dietrich Epp <depp@zdome.net>
2021-08-09 16:53:34 -0400

    demo: Create WebGL context

--- demo/main.js
+++ demo/main.js
@@ -1,1 +1,26 @@
-console.log('JavaScript code');
+function Fail(msg) {
+  const div = document.createElement('div');
+  const h = document.createElement('h2');
+  div.appendChild(h);
+  h.appendChild(document.createTextNode('Error'));
+  const p = document.createElement('p');
+  div.appendChild(p);
+  p.appendChild(document.createTextNode(msg));
+  document.body.appendChild(div);
+}
+
+function Start() {
+  const canvas = document.createElement('canvas');
+  const gl = canvas.getContext('webgl2', {
+    alpha: false,
+  });
+  if (gl == null) {
+    Fail('Could not create WebGL 2 context.');
+    return;
+  }
+  document.body.appendChild(canvas);
+  gl.clearColor(0.3, 0.5, 0.7, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+Start();

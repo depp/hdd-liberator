6686085c0e043244626b0b94b739236f6a4fb94a
Dietrich Epp <depp@zdome.net>
2021-08-09 12:09:50 -0400

    Create web server shell

--- devserver/main.go
+++ devserver/main.go
@@ -1,14 +1,78 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 )
 
+type handler struct{}
+
+func (*handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logrus.Infoln(r.URL)
+	http.NotFound(w, r)
+}
+
 func mainE() error {
-	return errors.New("unimplemented")
+	fHost := pflag.String("host", "localhost", "host to serve from")
+	fPort := pflag.Int("port", 9013, "port to serve from")
+	pflag.Parse()
+	if args := pflag.Args(); len(args) != 0 {
+		return fmt.Errorf("unexpected argument: %q", args[0])
+	}
+
+	ctx := context.Background()
+	log := logrus.StandardLogger()
+	rslv := net.DefaultResolver
+	addrs, err := rslv.LookupIPAddr(ctx, *fHost)
+	if err != nil {
+		return fmt.Errorf("could not look up host: %v", err)
+	}
+	h := handler{}
+	s := http.Server{
+		Handler:     &h,
+		BaseContext: func(_ net.Listener) context.Context { return ctx },
+	}
+	var root *url.URL
+	for _, addr := range addrs {
+		ta := net.TCPAddr{
+			IP:   addr.IP,
+			Zone: addr.Zone,
+			Port: *fPort,
+		}
+		l, err := net.ListenTCP("tcp", &ta)
+		if err != nil {
+			return err
+		}
+		if root == nil {
+			host := *fHost
+			if host == "" {
+				host = "localhost"
+			}
+			root = &url.URL{
+				Scheme: "http",
+				Host:   net.JoinHostPort(host, strconv.Itoa(*fPort)),
+				Path:   "/",
+			}
+			log.Infoln("Serving on:", root)
+		}
+		go func(l *net.TCPListener) {
+			err := s.Serve(l)
+			log.Fatalln("serve:", err)
+		}(l)
+	}
+	if root == nil {
+		return errors.New("no address to serve on")
+	}
+	select {}
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module moria.us/js13k
 
 go 1.15
 
-require github.com/sirupsen/logrus v1.8.1
+require (
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/pflag v1.0.5
+)

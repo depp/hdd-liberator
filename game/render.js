7047a252e9608406c48442fbdfa8d2e8189a033c
Dietrich Epp <depp@zdome.net>
2021-08-22 16:15:11 -0400

    Remove debug logging

--- game/render.js
+++ game/render.js
@@ -14,12 +14,13 @@ export function SetCanvasContext(c) {
  * @param {number} time
  */
 export function Render(time) {
+  const w = ctx.canvas.clientWidth;
+  const h = ctx.canvas.clientHeight;
   const t = time * 0.001;
   ctx.fillStyle = `rgb(${[
     128 + 128 * Math.cos(t),
     128 + 128 * Math.cos(t + (Math.PI * 2) / 3),
     128 + 128 * Math.cos(t - (Math.PI * 2) / 3),
   ]})`;
-  console.log(`Size: ${[ctx.canvas.clientWidth, ctx.canvas.clientHeight]}`);
-  ctx.fillRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+  ctx.fillRect(0, 0, w, h);
 }

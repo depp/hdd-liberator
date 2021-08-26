00de5c1e27f4fa78ad24dd2f36a3b9e861d909f5
Dietrich Epp <depp@zdome.net>
2021-08-26 15:50:52 -0400

    audio: Create a simple subtractive synth graph

--- game/audio.js
+++ game/audio.js
@@ -6,7 +6,7 @@ import { COMPO } from './common.js';
 let Ctx;
 
 /**
- * Play a soudn with the given buffer.
+ * Play a sound with the given buffer.
  * @param {AudioBuffer} buffer
  */
 function PlayBuffer(buffer) {
@@ -23,9 +23,24 @@ export function PlaySound() {
   if (!COMPO && !Ctx) {
     throw new Error('Ctx is null');
   }
+  const gain = Ctx.createGain();
+  gain.connect(Ctx.destination);
+  gain.gain.value = 1e-6;
+  gain.gain.exponentialRampToValueAtTime(1, 0.02);
+  gain.gain.exponentialRampToValueAtTime(1e-2, 1);
+
+  const filter = Ctx.createBiquadFilter();
+  filter.type = 'lowpass';
+  filter.connect(gain);
+  filter.frequency.value = 100;
+  filter.frequency.exponentialRampToValueAtTime(15e3, 0.1);
+  filter.frequency.exponentialRampToValueAtTime(100, 1);
+  filter.Q.value = 1.4;
+
   const osc = Ctx.createOscillator();
+  osc.connect(filter);
+  osc.type = 'sawtooth';
   osc.frequency.value = 440 * 2 ** (-9 / 12);
-  osc.connect(Ctx.destination);
   osc.start();
   osc.stop(Ctx.currentTime + 1);
 }

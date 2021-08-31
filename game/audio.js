b10acc30095556e5d25fece8ae718f32a07a5939
Dietrich Epp <depp@zdome.net>
2021-08-31 12:02:39 -0400

    audio: Create bytecode interpreter for audio
    The bytecode, when interpreted, creates a graph of audio nodes.

--- game/audio.js
+++ game/audio.js
@@ -41,6 +41,181 @@ function PlayBuffer(buffer) {
   source.start();
 }
 
+/**
+ * @param {!Array<number>} program
+ * @param {!AudioNode} out
+ * @param {number} t0 The program start time, seconds since start
+ * @param {number} tgate The length of the note, in seconds
+ * @param {number} note The MIDI note value to play
+ */
+function RunProgram(program, out, t0, tgate, note) {
+  /**
+   * Duration of the sound, in seconds.
+   * @type {number}
+   */
+  let duration = tgate;
+
+  /**
+   * All audio sources.
+   * @type {!Array<!OscillatorNode>}
+   */
+  const sources = [];
+
+  /**
+   * Exponent for calculating parameter values. Numbers encoded as N are decoded
+   * as exponent**N, multiplied by a scale factor. With a scale factor of 1.0,
+   * the range is 0.00057 - 1.0, or 66 dB.
+   * @const {number}
+   */
+  const exponent = 0.94;
+
+  /**
+   * Scale applied to frequency parameters, in Hertz. A frequency encoded as N
+   * is decoded as frequencyScale * exponent**N. Range: 9.3 Hz - 20 kHz.
+   * @const {number}
+   */
+  const frequencyScale = 20e3;
+
+  /**
+   * Scale applied to time parameters, in seconds. A time encoded as N is
+   * decoded as timeScale * exponent**N. Range: 9.3 ms - 20 s.
+   */
+  const timeScale = 20;
+
+  /**
+   * Position in the program.
+   * @type number
+   */
+  let pos = 0;
+
+  /**
+   * Set an audio parameter to follow an ADSR envelope.
+   * @param {!AudioParam} param
+   * @param {number} x0
+   * @param {number} x1
+   */
+  function ADSR(param, x0, x1) {
+    if (!COMPO && pos + 4 > program.length) {
+      throw new Error('program overrun');
+    }
+    // Envelope shapes
+    // x0 = minimum value
+    // xs = sustain level
+    // x1 = maximum value
+    // ta = attack time (time to rise from x0 to x1)
+    // td = decay time (time to decay from x1 to x0)
+    // tr = release time (time to release from x1 to x0)
+    //
+    // For long notes, tg > ta + td, the envelope is:
+    // (0, e) -> (ta, 1) -> (ta + td, xs) -> (tg, xs) -> (tg + tr, e)
+    let [a, d, s, r] = program.slice(pos, (pos += 4));
+    let ta = timeScale * exponent ** a;
+    let tr = timeScale * exponent ** r;
+    param.setValueAtTime(x0, t0);
+    param.exponentialRampToValueAtTime(x1, t0 + ta);
+    let tdgate = tgate - ta;
+    if (tdgate > 0) {
+      // Gate is released after envelope attack, so the delay stage will
+      // happen.
+      let tk = timeScale * exponent ** d;
+      let xs = (x1 / x0) ** (s / (NUM_VALUES - 1));
+      param.setTargetAtTime(xs, t0 + ta, tk);
+      param.setValueAtTime(xs + (x1 - xs) / Math.exp(tdgate / tk), t0 + tgate);
+      param.exponentialRampToValueAtTime(x0, t0 + tgate + tr);
+      duration = Math.max(duration, tgate + tr);
+    } else {
+      // Gate is released before envelope attack, so it releases
+      // immediately.
+      param.exponentialRampToValueAtTime(x0, t0 + ta + tr);
+      duration = Math.max(duration, ta + tr);
+    }
+  }
+
+  /** @type {!Array<!function(!AudioParam)>} */
+  const paramOpcodes = [
+    (_param) => {},
+    // Constant.
+    ...[1, timeScale, frequencyScale].map((scale) => (param) => {
+      if (!COMPO && pos + 1 > program.length) {
+        throw new Error('program overrun');
+      }
+      param.value = scale * exponent ** program[pos++];
+    }),
+    // Gain ADSR.
+    (param) => ADSR(param, exponent ** (NUM_VALUES - 1), 1),
+    // Frequency ADSR.
+    (param) => {
+      if (!COMPO && pos + 2 > program.length) {
+        throw new Error('program overrun');
+      }
+      ADSR(
+        param,
+        frequencyScale * exponent ** program[pos++],
+        frequencyScale * exponent ** program[pos++],
+      );
+    },
+    // Note value.
+    (param) => {
+      param.value = 440 * 2 ** ((note - 69) / 12);
+    },
+  ];
+
+  /**
+   * @param {!AudioNode} node
+   * @param {...!AudioParam} params
+   */
+  function AddNode(node, ...params) {
+    for (const param of params) {
+      if (!COMPO && pos + 1 > program.length) {
+        throw new Error('program overrun');
+      }
+      /** @type {number} */
+      const opcode = program[pos++];
+      /** @type {function(AudioParam!)?} */
+      const f = paramOpcodes[opcode];
+      if (!COMPO && f == null) {
+        throw new Error(`invalid param opcode: ${opcode}`);
+      }
+      f(param);
+    }
+    node.connect(out);
+    out = node;
+  }
+
+  /** @type {Array<function()!>} */
+  const opcodes = [
+    () => {
+      const node = Ctx.createGain();
+      AddNode(node, node.gain);
+    },
+    ...['lowpass', 'highpass', 'bandpass'].map((type) => () => {
+      const node = Ctx.createBiquadFilter();
+      node.type = type;
+      AddNode(node, node.frequency, node.detune, node.Q);
+    }),
+    ...['square', 'sawtooth', 'triangle'].map((type) => () => {
+      const node = Ctx.createOscillator();
+      node.type = type;
+      AddNode(node, node.frequency, node.detune);
+      sources.push(node);
+    }),
+  ];
+
+  while (pos < program.length) {
+    const opcode = program[pos++];
+    const f = opcodes[opcode];
+    if (!COMPO && f == null) {
+      throw new Error(`invalid opcode: ${opcode}`);
+    }
+    f();
+  }
+
+  for (const source of sources) {
+    source.start(t0);
+    source.stop(t0 + duration);
+  }
+}
+
 export function PlaySound() {
   if (!COMPO && !Ctx) {
     throw new Error('Ctx is null');
@@ -49,12 +224,24 @@ export function PlaySound() {
   if (!Songs) {
     return;
   }
+  /** @type {Array<number>!} */
+  const program = [
+    // gain, adsr
+    0, 4, 112, 85, 11, 40,
+    // lowpass, adsr, skip, skip, skip
+    1, 5, 85, 11, 85, 85, 11, 40, 0, 0,
+    // oscillator
+    5, 6, 0,
+  ];
   const song = Songs[0];
   if (!song) {
     return;
   }
   const ticksize = song.tickDuration;
   const t0 = Ctx.currentTime;
+  const gain = Ctx.createGain();
+  gain.gain.value = 0.2;
+  gain.connect(Ctx.destination);
   for (const track of song.tracks) {
     const { values, durations } = track;
     let t = t0;
@@ -62,28 +249,8 @@ export function PlaySound() {
       const value = values[i];
       const duration = durations[i];
 
-      if (value != 0x80) {
-        const gain = Ctx.createGain();
-        gain.connect(Ctx.destination);
-        gain.gain.setValueAtTime(1e-6, t);
-        gain.gain.exponentialRampToValueAtTime(1, t + 0.02);
-        gain.gain.exponentialRampToValueAtTime(1e-2, t + 1);
-
-        const filter = Ctx.createBiquadFilter();
-        filter.type = 'lowpass';
-        filter.connect(gain);
-        filter.frequency.setValueAtTime(100, t);
-        filter.frequency.exponentialRampToValueAtTime(15e3, t + 0.1);
-        filter.frequency.exponentialRampToValueAtTime(100, t + 1);
-        filter.Q.value = 1.4;
-
-        const osc = Ctx.createOscillator();
-        osc.detune.value = 5 * (Math.random() - 0.5);
-        osc.connect(filter);
-        osc.type = 'sawtooth';
-        osc.frequency.value = 440 * 2 ** ((value - 69) / 12);
-        osc.start(t);
-        osc.stop(t + 1);
+      if (value > 0) {
+        RunProgram(program, gain, t, duration * ticksize, value);
       }
 
       t += duration * ticksize;
@@ -146,7 +313,7 @@ export function LoadMusic(data) {
       switch (byte) {
         case NUM_VALUES - 2:
           // rest
-          values.push(128);
+          values.push(-1);
           break;
         case NUM_VALUES - 1:
           // track end
